541adb8ac0ac87752fe340a218b497a982c91440
Vyas Raina <sayvaniar@gmail.com>
2022-01-28 09:32:12 +0000

    no exception

--- models.py
+++ models.py
@@ -7,12 +7,12 @@ import torch.nn as nn
 
 class T5Based(nn.Module):
     def __init__(self, size='t5-base'):
-
-        # try:
-        self.tokenizer = T5Tokenizer.from_pretrained(size)
-        self.model = T5ForConditionalGeneration.from_pretrained(size)
-        # except:
-        #     raise ValueError(f"T5 size {size} unsupported")
+        super().__init__()
+        try:
+            self.tokenizer = T5Tokenizer.from_pretrained(size)
+            self.model = T5ForConditionalGeneration.from_pretrained(size)
+        except:
+            raise ValueError(f"T5 size {size} unsupported")
 
     
     def forward(self, input_ids=None, attention_mask=None, labels=None):

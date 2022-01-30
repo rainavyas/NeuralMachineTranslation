69d9e0f4f8734c71b7521e0e2013f16b4d0a0827
Vyas Raina <sayvaniar@gmail.com>
2022-01-30 20:17:31 +0000

    debug

--- data_prep.py
+++ data_prep.py
@@ -23,8 +23,17 @@ class DataTensorLoader():
 
     def _get_data(self, data, return_sentences=False, num_points=-1):
 
-        source_sentences = [item['translation'][self.source] for item in data]
-        target_sentences = [item['translation'][self.target] for item in data]
+        if num_points == -1: num_points = len(data)
+        source_sentences = []
+        target_sentences = []
+        for num in range(num_points):
+            item = data[num]
+            print(item['translation'][self.source])
+            source_sentences.append(item['translation'][self.source])
+            target_sentences.append(item['translation'][self.target])
+
+        # source_sentences = [item['translation'][self.source] for item in data)]
+        # target_sentences = [item['translation'][self.target] for item in data]
 
         if num_points != -1:
             source_sentences = source_sentences[:num_points]
